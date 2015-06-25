86752ad48744ea621a776a69bb6a219dae42a2cd
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-24 22:51:39 -0400

    Console.log the track

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    SC.initialize({
+      client_id: "f3295f8663e05856bfced82ece0ebc96"
+    });
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      SC.get('/tracks/128350315', function(song) {
+          resolve(song);
+          console.log(song);
+      });
+    });
+  }
 });
